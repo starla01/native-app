46c5a2af6d6bf941d6d08cbcc8b0c6e2f14631c9
“Fernando <starla.frr@gmail.com>
2018-02-12 18:50:24 -0600

    Winner Cat

--- App.js
+++ App.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
-var data = ['','','','','','','','',''];
+import { StyleSheet, Text, View, Button, Alert, TouchableHighlight, AppRegistry } from 'react-native';
+var squares = Array(9).fill(null);
+var tiro = 'X';
+var count = 0;
 // Class BOARD
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+  setSquareValue = (value, pos) => {
+    squares[pos] = value;
+    this.setState(previousState => {
+        return { squares: squares };
+    });
+    this.calculateWinner(squares)
+  }
+  calculateWinner(s){
+    const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+  }
   renderSquare() {
-      var s = data.map(function(val, key){
-        return <Square value={key} key={key}/>
+    var _this = this;
+    var s = this.state.squares.map(function(val, key){
+        return <Square value={val} pos={key} key={key} setSquareValue={_this.setSquareValue}/>
       })
     return s;
   }
   render(){
+    const winner = this.calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+      Alert.alert(status)
+    } 
     return (<View style={styles.board}>
         {this.renderSquare()}
       </View>)
@@ -18,19 +59,28 @@ class Board extends React.Component {
 }
 
 // Class SQUARE
-class Square extends React.Component {
+class Square extends Component {
+  constructor(props){
+    super(props);
+  }
   _onPressButton(k){
-    Alert.alert("Posición: " + (k+1))
+    count = count + 1;
+    if(count % 2 === 0){
+        tiro = 'X';
+    }else{
+        tiro = 'O';
+    }
+
+    if(typeof this.props.setSquareValue(tiro, k) === 'function'){
+      this.props.setSquareValue(tiro, k);
+    }
   }
   render() {
-    let turno = data[this.props.value]
     return (
           <View style={styles.square}>
-            <Button
-            onPress={() => this._onPressButton(this.props.value)}
-            title={"Press"}
-            color="#841584"
-          />
+          <TouchableHighlight style={styles.button} onPress={() => this._onPressButton(this.props.pos)}>
+            <Text style={styles.textButton}>{this.props.value}</Text>
+          </TouchableHighlight>
           </View>
     );
   }
@@ -71,10 +121,21 @@ const styles = StyleSheet.create({
   square: {
     width: 100,
     height: 100,
-    backgroundColor: 'pink',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 6,
+    borderRadius: 6
+  },
+  button: {
+      width: 100,
+      height: 100,
+      alignItems: 'center',
+      backgroundColor: '#2196F3',
+      borderRadius: 6,
+      display: 'flex',
+      justifyContent: 'center'
+    },
+  textButton: {
+    fontSize: 40
   }
 });
